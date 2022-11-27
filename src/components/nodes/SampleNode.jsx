0bc1a4203c375e34dcc12c93107bec265f4105c7
Samuel Degueldre <sad@odoo.com>
2022-11-27 13:57:41 +0100

    :bug: fix sample node causing a crash when choosing a new sample
    This is caused by the fact that the buffer on an AudioBufferSourceNode
    can only be set once. To fix this, the key of the underlying
    AudioBufferSourceNode is computed based on a version number in addition
    to the key of the ReactFlow node.

--- src/components/nodes/SampleNode.jsx
+++ src/components/nodes/SampleNode.jsx
@@ -24,8 +24,8 @@ export default function SampleNode({ id, data = defaults }) {
         // updateNode after the component has been destroyed.
         const rawBuffer = await ev.target.files[0].arrayBuffer()
         const audioBuffer = await ctx.decodeAudioData(rawBuffer)
-        updateNode(id, { ...data, sample: audioBuffer })
-    }, []);
+        updateNode(id, { ...data, sample: audioBuffer, version: data.version + 1 })
+    }, [data.version]);
 
     return (
         <BaseNode name={name} info={info} inputs={inputs} outputs={outputs} colour={{ from: "from-indigo-500", to: "to-blue-400" }}>
@@ -39,6 +39,10 @@ export default function SampleNode({ id, data = defaults }) {
 
 export const defaults = {
     sample: null,
+    // This field holds the "version" of the sample because the buffer of an AudioBufferSourceNode
+    // can only be set once, whenever the sample changes, we increment the version number so that
+    // a new audio node is created with the new buffer because the version is part of the key.
+    version: 0,
 }
 
 export const asReactFlowNode = (id, data = defaults, opts = {}) => ({
@@ -46,7 +50,7 @@ export const asReactFlowNode = (id, data = defaults, opts = {}) => ({
 })
 
 export const asAudioNodes = (id, data = defaults, connections = {}) => [
-    Audio.keyed(id,
+    Audio.keyed(`${id}-${data.version}`,
         Audio.node("AudioBufferSourceNode", [{ type: "NodeProperty", label: "buffer", value: data.sample }],
             connections?.[id] ?? []
         )
