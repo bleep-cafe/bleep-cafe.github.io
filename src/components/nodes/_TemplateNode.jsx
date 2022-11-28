b1b4711505ad92eceaac7453012df7f3bcd14d51
Hayleigh Thompson <me@hayleigh.dev>
2022-11-28 15:51:18 +0000

    :art: Prettier making things pretty.

--- src/components/nodes/_TemplateNode.jsx
+++ src/components/nodes/_TemplateNode.jsx
@@ -1,14 +1,15 @@
 // The module is provided as a template for impementing new audio nodes in the
 // ReactFlow graph. Do *not* import this module directly. Instead, copy it to
 // a new file in the same directory, and modify it to suit your needs.
-import * as React from "react"
+import * as Audio from '../../audio/context'
+import * as React from 'react'
 
-import BaseNode from "./BaseNode"
+import BaseNode from './BaseNode'
 
 // CONSTANTS -------------------------------------------------------------------
 
-const name = ""
-const info = "" /*
+const name = ''
+const info = '' /*
     // When `info` isn't empty, it will be displayed in a popover if the user
     // clicks the "?" icon in the top right corner of the node.
     //
@@ -21,11 +22,11 @@ const info = "" /*
 
 // The `inputs` and `outputs` arrays define the number and names of the node's
 // input and output ports. The names are used to generate the labels for the
-// ports, and also used when converting the node to Web Audio bits. 
+// ports, and also used when converting the node to Web Audio bits.
 //
 // An empty name implicitly means the port is named after the node's ID. Names
 // beginning with a period, such as ".frequency", map to an audio node's parameters.
-// It's clearer to just see how this works, so let's take a quick look: 
+// It's clearer to just see how this works, so let's take a quick look:
 //
 // ""               -> signal.connect(nodes[id])
 // ".gain"          -> signal.connect(nodes[id].gain)
@@ -54,9 +55,10 @@ export default function _TemplateNode({ id, data = defaults }) {
 // CONSTUCTORS -----------------------------------------------------------------
 
 export const asReactFlowNode = (id, data = defaults, opts = {}) => ({
-    type: _TemplateNode.name, id, data, ...opts
+    type: _TemplateNode.name,
+    id,
+    data,
+    ...opts,
 })
 
-export const asAudioNodes = (id, data = defaults, connections = {}) => [
-
-]
+export const asAudioNodes = (id, data = defaults, connections = {}) => []
